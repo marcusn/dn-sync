d1538b7e3ab5a5ebb9d485346ae2dc492b790259
Marcus Nilsson <mni@appearnetworks.com>
2011-03-29 10:22:14 +0200

    Preference item

--- src/tv/nilsson/dnsync/MyActivity.java
+++ src/tv/nilsson/dnsync/MyActivity.java
@@ -1,15 +1,16 @@
 package tv.nilsson.dnsync;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class MyActivity extends Activity
+public class MyActivity extends PreferenceActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        addPreferencesFromResource(R.xml.settings);
     }
 }
