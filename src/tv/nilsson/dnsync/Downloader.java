0794eef3ba0f970548d6dec19370b7c9f8fc2cc0
Marcus Nilsson <mni@appearnetworks.com>
2011-03-30 17:16:59 +0200

    Extracted Downloader class

---
+++ src/tv/nilsson/dnsync/Downloader.java
@@ -0,0 +1,107 @@
+package tv.nilsson.dnsync;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Downloader {
+  private static String SERVICE_ENDPOINT = "http://pdf.dn.se/dn-ssf/pdf/archive.jsp";
+  private static String LOGIN_ENDPOINT = "http://pdf.dn.se//dn-ssf/j_spring_security_check";
+
+  private String customerNr;
+  private String email;
+
+  public static class DownloadInfo {
+    public Uri uri;
+    public String filename;
+
+    public DownloadInfo(Uri uri, String filename) {
+      this.uri = uri;
+      this.filename = filename;
+    }
+  }
+
+  public Downloader(String customerNr, String email) {
+    this.customerNr = customerNr;
+    this.email = email;
+  }
+
+  public DownloadInfo obtainDownloadInfo() {
+    try {
+      Uri downloadUri = getDownloadUri(doLogin());
+      return new DownloadInfo(downloadUri, extractFilename(downloadUri));
+    }
+    catch(IOException e) {
+      return null;
+    }
+  }
+
+  private String doLogin() throws IOException {
+    HttpClient httpClient = new DefaultHttpClient();
+    Uri.Builder uriBuilder = Uri.parse(LOGIN_ENDPOINT).buildUpon();
+
+    HttpGet request = new HttpGet(uriBuilder.appendQueryParameter("j_username", customerNr).appendQueryParameter("j_password", email).toString());
+    HttpResponse response = httpClient.execute(request);
+
+    int status = response.getStatusLine().getStatusCode();
+    if (status != 200) return null;
+
+    String s = extractEntity(response);
+
+    Pattern pattern = Pattern.compile(";jsessionid=(.*?)\"");
+
+    Matcher matcher = pattern.matcher(s);
+    if (matcher.find()) {
+      String jsessionId = matcher.group(1);
+
+      return jsessionId;
+    }
+
+    return null;
+  }
+
+  private String extractEntity(HttpResponse response) throws IOException {ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    response.getEntity().writeTo(byteArrayOutputStream);
+    return new String(byteArrayOutputStream.toByteArray());
+  }
+
+  private Uri getDownloadUri(String sessionId) throws IOException {
+    HttpClient httpClient = new DefaultHttpClient();
+    Uri.Builder uriBuilder = Uri.parse(SERVICE_ENDPOINT + ";jsessionid=" + sessionId).buildUpon().appendQueryParameter("date", "latest");
+
+
+    HttpGet request = new HttpGet(uriBuilder.toString());
+    HttpResponse response = httpClient.execute(request);
+
+    if (response.getStatusLine().getStatusCode() != 200) return null;
+
+    String s = extractEntity(response);
+
+    Pattern pattern = Pattern.compile("\"(.*?DN\\.pdf.*?)\"");
+
+    Matcher matcher = pattern.matcher(s);
+    if (matcher.find()) {
+      String link = matcher.group(1);
+      return Uri.parse("http://pdf.dn.se" + link);
+    }
+
+    return null;
+  }
+
+  private static String extractFilename(Uri downloadUri) {
+
+    Matcher filenameMatcher = Pattern.compile("(.*?);jsessionid").matcher(downloadUri.getLastPathSegment());
+    return filenameMatcher.find() ? filenameMatcher.group(1) : null;
+  }
+}
--- src/tv/nilsson/dnsync/SyncService.java
+++ src/tv/nilsson/dnsync/SyncService.java
@@ -2,29 +2,20 @@ package tv.nilsson.dnsync;
 
 import android.app.DownloadManager;
 import android.app.IntentService;
-import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SyncService extends IntentService {
   private static final String TAG = "SyncService";
   public static final String ACTION_SYNC = "tv.nilsson.dnsync.SYNC";
-  private static String SERVICE_ENDPOINT = "http://pdf.dn.se/dn-ssf/pdf/archive.jsp";
-  private static String LOGIN_ENDPOINT = "http://pdf.dn.se//dn-ssf/j_spring_security_check";
 
   public SyncService() {
     super(TAG);
@@ -43,87 +34,28 @@ public class SyncService extends IntentService {
     }
   }
 
-  private static String login(String customerNr, String email) throws IOException {
-    HttpClient httpClient = new DefaultHttpClient();
-    Uri.Builder uriBuilder = Uri.parse(LOGIN_ENDPOINT).buildUpon();
-
-    HttpGet request = new HttpGet(uriBuilder.appendQueryParameter("j_username", customerNr).appendQueryParameter("j_password", email).toString());
-    HttpResponse response = httpClient.execute(request);
-
-    int status = response.getStatusLine().getStatusCode();
-    if (status != 200) return null;
-
-    String s = extractEntity(response);
-
-    Pattern pattern = Pattern.compile(";jsessionid=(.*?)\"");
-
-    Matcher matcher = pattern.matcher(s);
-    if (matcher.find()) {
-      String jsessionId = matcher.group(1);
-
-      return jsessionId;
-    }
-
-    return null;
-  }
-
-  private static String extractEntity(HttpResponse response) throws IOException {ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    response.getEntity().writeTo(byteArrayOutputStream);
-    return new String(byteArrayOutputStream.toByteArray());
-  }
-
-  private static Uri getDownloadUri(String sessionId) throws IOException {
-    HttpClient httpClient = new DefaultHttpClient();
-    Uri.Builder uriBuilder = Uri.parse(SERVICE_ENDPOINT + ";jsessionid=" + sessionId).buildUpon().appendQueryParameter("date", "latest");
-
-
-    HttpGet request = new HttpGet(uriBuilder.toString());
-    HttpResponse response = httpClient.execute(request);
-
-    if (response.getStatusLine().getStatusCode() != 200) return null;
-
-    String s = extractEntity(response);
-
-    Pattern pattern = Pattern.compile("\"(.*?DN\\.pdf.*?)\"");
-
-    Matcher matcher = pattern.matcher(s);
-    if (matcher.find()) {
-      String link = matcher.group(1);
-      return Uri.parse("http://pdf.dn.se" + link);
-    }
-
-    return null;
-  }
-
   public void download(String customerNr, String email) throws IOException {
-    String sessionId = login(customerNr, email);
-    if (sessionId == null) return;
+    Downloader downloader = new Downloader(customerNr, email);
 
-    Uri downloadUri = getDownloadUri(sessionId);
-    if (downloadUri == null) return;
+    Downloader.DownloadInfo downloadInfo = downloader.obtainDownloadInfo();
 
-    String filename = extractFilename(downloadUri);
+    if (downloadInfo == null) {
+      Toast.makeText(this, "DN Download failed", Toast.LENGTH_SHORT).show();
+      return;
+    }
+
     File downloads = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-
+    //noinspection ResultOfMethodCallIgnored
     downloads.mkdir();
 
-    File file = new File(downloads, filename);
+    File file = new File(downloads, downloadInfo.filename);
 
     if (file.exists()) return;
 
     DownloadManager downloadMananger = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 
-    //downloadUri = Uri.parse("http://www.google.se");
+    //downloadInfo.uri = Uri.parse("http://www.google.se");
 
-    downloadMananger.enqueue(new DownloadManager.Request(downloadUri).setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename));
-
+    downloadMananger.enqueue(new DownloadManager.Request(downloadInfo.uri).setDestinationUri(Uri.fromFile(file)));
   }
-
-  private static String extractFilename(Uri downloadUri) {
-
-    Matcher filenameMatcher = Pattern.compile("(.*?);jsessionid").matcher(downloadUri.getLastPathSegment());
-    return filenameMatcher.find() ? filenameMatcher.group(1) : null;
-  }
-
-
 }
